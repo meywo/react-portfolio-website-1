9132703ce3d2db5156f234ce4507d30c9581e245
meywo <wong.meyen@gmail.com>
2022-10-05 19:51:43 +1300

    3rd commit-oct

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CV from '../../assets/cv.pdf'
+
+const CTA = () => {
+  return (
+    <div className="CTA">
+        <a href={CV} download>Download CV</a>
+        <a href="#contact">Let's Talk</a>
+    </div>
+  )
+}
+
+export default CTA
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header_container"></div>
+    <h5>Hello I'm </h5>
+    <h1>Hajia Bintu</h1>
+    <h5 className="text-light">Fullstack Developer</h5>
+    <CTA/>
+    </header>
   )
 }
 
