18805f5560989067f4b4775c92e23b3e5bef7440
meywo <wong.meyen@gmail.com>
2022-10-12 23:15:15 +1300

    12th commit - oct

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -5,7 +5,28 @@ import AVTR2 from '../../assets/avatar2.jpg'
 import AVTR3 from '../../assets/avatar3.jpg'
 import AVTR4 from '../../assets/avatar4.jpg'
 
-
+const data = [
+  {
+    avatar: AVTR1,
+    name: 'Tina Snow',
+    review:'Lorem ipsum dolor sit amet consectetur adipisicing elit.reprehenderit?'
+  },
+  {
+    avatar: AVTR2,
+    name: 'Shatta Wale',
+    review:'Lorem ipsum dolor sit amet consectetur adipisicing elit.reprehenderit?'
+  },
+{
+  avatar: AVTR3,
+    name: 'Kwame Despite',
+    review:'Lorem ipsum dolor sit amet consectetur adipisicing elit.reprehenderit?'
+},
+{
+  avatar: AVTR4,
+    name: 'Nana Arna McBrun',
+    review:'Lorem ipsum dolor sit amet consectetur adipisicing elit.reprehenderit?'
+}
+]
 
 const Testimonials = () => {
   return (
@@ -13,43 +34,20 @@ const Testimonials = () => {
       <h5>Review from clients</h5>
       <h2>Testimonials</h2>
     
-    <div className="container testimonial_container">
-      <article className="testimonial">
-        <div className="client_avatar">
-          <img src={AVTR1} alt="Avatar One" />
-          </div>
-          <h5 className='client_name'>Ernest Achiever</h5>
-          <small className='client_review'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. At eveniet deleniti dolorum sit quae unde obcaecati 
-          tempora quas porro enim? Modi error doloribus itaque accusantium fugit cupiditate nobis incidunt explicabo!
-          </small>
-      </article>
-      <article className="testimonial">
-        <div className="client_avatar">
-          <img src={AVTR2} alt="Avatar One" />
-          </div>
-          <h5 className='client_name'>Ernest Achiever</h5>
-          <small className='client_review'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. At eveniet deleniti dolorum sit quae unde obcaecati 
-          tempora quas porro enim? Modi error doloribus itaque accusantium fugit cupiditate nobis incidunt explicabo!
-          </small>
-      </article>
-      <article className="testimonial">
-        <div className="client_avatar">
-          <img src={AVTR3} alt="Avatar One" />
-          </div>
-          <h5 className='client_name'>Ernest Achiever</h5>
-          <small className='client_review'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. At eveniet deleniti dolorum sit quae unde obcaecati 
-          tempora quas porro enim? Modi error doloribus itaque accusantium fugit cupiditate nobis incidunt explicabo!
-          </small>
-      </article>
-      <article className="testimonial">
-        <div className="client_avatar">
-          <img src={AVTR4} alt="Avatar One" />
-          </div>
-          <h5 className='client_name'>Ernest Achiever</h5>
-          <small className='client_review'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. At eveniet deleniti dolorum sit quae unde obcaecati 
-          tempora quas porro enim? Modi error doloribus itaque accusantium fugit cupiditate nobis incidunt explicabo!
-          </small>
-      </article>
+    <div className="container testimonials_container">
+      {
+        data.map(({avatar,name,review}, index)  => {
+          return(
+            <article key={index} className="testimonial">
+            <div className="client_avatar">
+              <img src={avatar} />
+              </div>
+              <h5 className='client_name'>{name}</h5>
+              <small className='client_review'>{review}</small>
+             </article>
+          )
+        })
+      }
     </div>
     </section>
   )
