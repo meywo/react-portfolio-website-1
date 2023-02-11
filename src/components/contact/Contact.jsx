5daf741f457ae81a930033338768d83dc68a3a66
meywo <wong.meyen@gmail.com>
2023-02-11 19:58:08 +1300

    3rd commit feb

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,7 +4,7 @@ import {MdOutlineEmail} from 'react-icons/md'
 import {RiMessengerLine} from 'react-icons/ri'
 import {BsWhatsapp} from 'react-icons/bs'
 import { useRef } from 'react';
-import emailjs from 'emailjs/browser'
+import emailjs from 'emailjs-com'
 
 
 const Contact = () => {
