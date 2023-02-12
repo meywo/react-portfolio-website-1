75ad362643b9922c1162768672d22debba26d35e
meywo <wong.meyen@gmail.com>
2023-02-13 00:12:35 +1300

    9 commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import sidelady from '../../assets/sidelady.jpg'
+import sidelady from '../../assets/sidelady.png'
 import HeaderSocial from './HeaderSocials'
 
 const Header = () => {
